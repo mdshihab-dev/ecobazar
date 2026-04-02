29642b6759abc0b183aa4309a0a110ab4ac3e195
Md-Shihab247 <sheikhmdsihab1@gmail.com>
2026-04-02 11:42:12 +0600

    footer top responsive done / all devices

--- src/components/footer/FooterTop.tsx
+++ src/components/footer/FooterTop.tsx
@@ -14,21 +14,21 @@ const icons : IconType[] = [
 
 const FooterTop = () => {
   return (
-    <section className=" bg-light-blue px-2 md:px-0 py-5 md:py-8 lg:py-10">
-      <div className=" container flex flex-col md:flex-row items-center justify-between">
-          <div className=" max-w-md text-center md:text-start">
-            <h2 className="font-poppins font-semibold text-xl md:text-2xl leading-[150%] text-dark">
+    <section className=" bg-light-blue px-2 md:px-3 xl:px-0 py-5 md:py-8 lg:py-10">
+      <div className=" container flex flex-col lg:flex-row items-center lg:gap-x-3 justify-between">
+          <div className=" lg:w-[380] xl:max-w-md text-center lg:text-start">
+            <h2 className="font-poppins font-semibold text-xl lg:text-2xl leading-[150%] text-dark">
               Subcribe our Newsletter
             </h2>
-            <p className=" mt-1 px-5 md:px-0 font-poppins font-normal text-[14px] leading-[150%] text-secondary">
+            <p className=" mt-1 px-5 md:px-47 lg:px-0 font-poppins font-normal text-sm lg:text-[14px] leading-[150%] text-secondary">
               Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna.
             </p>
           </div>
 
-          <div className=" flex flex-col md:flex-row items-center justify-center gap-x-10">
+          <div className=" flex flex-col lg:flex-row items-center justify-center lg:gap-x-5 xl:gap-x-10">
             <div>
-            <div className=" my-3 md:my-0 flex items-center border border-[#e5e5e5] h-13 bg-white rounded-full">
-              <input type="text" placeholder="Your email address" className=" w-[60vw] md:w-134 py-2.5 md:py-3.5 px-3 md:px-6 font-normal font-poppins text-sm md:text-base leading-[150%] text-light-gray outline-hidden"/>
+            <div className=" my-3 md:my-5 flex items-center border border-[#e5e5e5] h-13 bg-white rounded-full">
+              <input type="text" placeholder="Your email address" className=" w-[60vw] lg:w-[300px] py-2.5 md:py-3.5 px-3 md:px-6 font-normal font-poppins text-sm md:text-base leading-[150%] text-light-gray outline-hidden"/>
               <Button text="Subscribe" styles=" px-6 md:px-10 py-4 bg-primary text-white rounded-full font-semibold text-sm md:text-base leading-[20px]"/>
             </div>
           </div>
@@ -36,7 +36,7 @@ const FooterTop = () => {
           <div className=" flex items-center gap-x-2">
             {
               icons.map((icon, index) => 
-                <div key={index} className="cursor-pointer h-10 w-10 group hover:bg-transparent border border-transparent hover:border-sub-secondary transition-all duration-500 bg-primary rounded-full flex items-center justify-center">
+                <div key={index} className="cursor-pointer size-10 group hover:bg-transparent border border-transparent hover:border-sub-secondary transition-all duration-500 bg-primary rounded-full flex items-center justify-center">
                   <span className=" text-white group-hover:text-sub-secondary transition-all duration-500">
                     {icon}
                   </span>
